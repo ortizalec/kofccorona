6cf873ee6aba57fd340e43ba32eabd8c205258d1
ortizalec <ortizalec@icloud.com>
2022-03-08 23:31:43 +0000

    Added Section 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,39 +3,43 @@
         <title>Knights Of Columbus, Corona</title>
 </svelte:head>
 
-<div class="flex flex-col justify-center py-5 mx-auto w-full md:w-5/6">
+<div class="flex flex-col gap-32 justify-center p-5 py-5 mx-auto w-full md:w-11/12 md:px-0">
         <section class="flex overflow-hidden flex-wrap rounded-xl">
-                <div class="w-full md:w-1/2 bg-darkgray">
-                        <h1 class="text-4xl font-bold text-white">
-                                Knights of Columbus Corona 
-                        </h1> 
-                        <h2 class="text-2xl font-normal text-white">
-                                Council #12345
-                        </h2>
+                <div class="flex flex-col justify-center items-center w-full md:w-1/2 bg-darkgray h-[50vh] md:h-[80vh]">
+                        <div class="flex flex-col p-5 md:p-0">
+                                <h1 class="mb-4 text-4xl font-bold text-white">
+                                        Knights of Columbus Corona 
+                                </h1> 
+                                <h2 class="mb-10 text-xl font-normal text-white">
+                                        Upholding the principles of charity, unity, and faith.
+                                </h2>
+                                <button class="py-4 w-full text-2xl font-bold text-white rounded-lg bg-gold">
+                                        Learn More
+                                </button>                  
+                        </div>
+
                 </div>
                 <div class=" bg-[url('catholic-building.jpg')] 
-                bg-cover h-[70vh] w-full md:w-1/2">
-                        
-                </div>
-        </section>
-        <section class=" bg-[url('catholic-building.jpg')] 
-        bg-cover h-[70vh] relative grid place-items-center
-        rounded-xl overflow-hidden">
-                <div class="absolute top-0 w-full h-full bg-black opacity-60">
+                bg-cover h-[50vh] md:h-[80vh] w-full md:w-1/2">
                 </div>
 
         </section>
-        <section>
-                <div>
-                        <h1 class="mb-5 text-3xl play"> Who we are</h1>
-                        <p class="font-sans text-xl">
-                                The Knights of Columbus is the world's largest Catholic 
-                                family fraternal service organization with 1.8 million members. 
-                                It provides members and their families with volunteer 
-                                opportunities in service to the Catholic Church, their 
-                                communities, families, and young people.
-                        </p>
-                        
+        <section class="flex justify-center w-full md:px-16">
+                <div class="flex flex-wrap justify-between">
+                        <div class="mb-5 w-full md:w-2/5 md:mb-0">
+                                <h2 class="mb-4 text-2xl">Who we are</h2>
+                                <h1 class="mb-8 text-4xl font-semibold">Building a bridge back to faith</h1>
+                                <p class="font-sans text-xl">
+                                        The Knights of Columbus is the world's largest Catholic 
+                                        family fraternal service organization with 1.8 million members. 
+                                        It provides members and their families with volunteer 
+                                        opportunities in service to the Catholic Church, their 
+                                        communities, families, and young people.
+                                </p>
+                        </div>
+                        <div class="w-full md:w-2/5">
+                                <img src="bridge.jpg" alt="" class="w-full h-full rounded-lg shadow">
+                        </div>               
                 </div>
         </section>
 </div>
