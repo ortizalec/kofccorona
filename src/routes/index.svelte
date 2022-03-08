d094767a046979de7ffc0973beff1a4d1207f6aa
ortizalec <ortizalec@icloud.com>
2022-03-08 03:57:37 +0000

    Added Paragraph with sample content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
 <h1 class="text-3xl font-bold underline">
         Knights of Columbus, Corona
 </h1>
+
+<p>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, ipsum voluptatum explicabo commodi cumque facilis. Sunt, nostrum cum illo rem, amet ab ea quasi ut, ad aliquam totam ullam quidem!
+</p>
